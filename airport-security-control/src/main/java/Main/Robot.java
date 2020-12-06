c5b479493419488503b1709141f96a7e3511f38c
FabMaster2000 <68748739+FabMaster2000@users.noreply.github.com>
2020-12-06 19:12:32 +0100

    Commit

--- airport-security-control/src/main/java/Main/Robot.java
+++ airport-security-control/src/main/java/Main/Robot.java
@@ -7,8 +7,8 @@ public class Robot {
     public DestroyedBaggage destroy(Baggage baggage) {
         char[][] destroyed = new char[1000][50];
         var destruction = 0;
-        for (int i = 0; i < baggage.getLayers().length; i++) {
-            var currentLayer = baggage.getLayers()[i];
+        for (int x = 0; x < baggage.getLayers().length; x++) {
+            var currentLayer = baggage.getLayers()[x];
             for (var j = 0; j < currentLayer.getContent().length - 50; j = j + 50) {
                 destroyed[destruction] = String.valueOf(currentLayer.getContent(), j, 50).toCharArray();
                 destruction++;
