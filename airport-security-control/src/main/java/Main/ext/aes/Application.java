83d3cb595cc745b44d62e35dccf758c96cee8e49
FabMaster2000 <68748739+FabMaster2000@users.noreply.github.com>
2020-12-06 18:42:11 +0100

    Commit

--- airport-security-control/src/main/java/Main/ext/aes/Application.java
+++ airport-security-control/src/main/java/Main/ext/aes/Application.java
@@ -1,16 +1,18 @@
 package Main.ext.aes;
 
-import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.Cipher;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
-import java.util.Arrays;
 import java.util.Base64;
+import java.util.Arrays;
 
-public class Application {
+public class Application
+{
     private SecretKeySpec secretKey;
 
-    public static void main(String... args) {
+    public static void main(String... args)
+    {
         Application application = new Application();
 
         final String secretKey = "dhbw$20^20_";
@@ -18,48 +20,58 @@ public class Application {
         String plainMessage = "***I***0614***";
         String encryptedString = application.encrypt(plainMessage, secretKey);
         String decryptedString = application.decrypt(encryptedString, secretKey);
-
         System.out.println(plainMessage);
         System.out.println(encryptedString);
         System.out.println(decryptedString);
     }
 
-    public String decrypt(String encryptedMessage, String key) {
-        try {
+    public String decrypt(String encryptedMessage, String key)
+    {
+        try
+        {
             setKey(key);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
             return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedMessage)));
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             System.out.println(e.getMessage());
         }
         return null;
     }
 
-    public void setKey(String inputKey) {
+    public void setKey(String inputKey)
+    {
         MessageDigest sha;
 
-        try {
+        try
+        {
             byte[] key = inputKey.getBytes(StandardCharsets.UTF_8);
             sha = MessageDigest.getInstance("SHA-1");
             key = sha.digest(key);
             key = Arrays.copyOf(key, 16);
             secretKey = new SecretKeySpec(key, "AES");
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             System.out.println(e.getMessage());
         }
     }
 
-    public String encrypt(String plainMessage, String key) {
-        try {
+    public String encrypt(String plainMessage, String key)
+    {
+        try
+        {
             setKey(key);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
             return Base64.getEncoder().encodeToString(cipher.doFinal(plainMessage.getBytes(StandardCharsets.UTF_8)));
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             System.out.println(e.getMessage());
         }
-
         return null;
     }
 }
