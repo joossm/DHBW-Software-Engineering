8062fcd3aeed7c4a8bbb0e1ae74c349ef10c6b52
joossm <jooss.matthias@gmail.com>
2020-12-06 18:51:45 +0100

    exp

--- airport-security-control/src/main/java/Main/ext/advancedEncryptionStandard/Application.java
+++ airport-security-control/src/main/java/Main/ext/advancedEncryptionStandard/Application.java
@@ -11,67 +11,38 @@ public class Application
 {
     private SecretKeySpec secretKey;
 
-    public static void main(String... args)
-    {
+    public static void main(String... args) throws Exception {
         Application application = new Application();
 
         final String secretKey = "dhbw$20^20_";
 
         String plainMessage = "***I***0614***";
         String encryptedString = application.encrypt(plainMessage, secretKey);
-        String decryptedString = application.decrypt(encryptedString, secretKey);
-        System.out.println(plainMessage);
-        System.out.println(encryptedString);
-        System.out.println(decryptedString);
+        application.decrypt(encryptedString, secretKey);
+
     }
 
-    public String decrypt(String encryptedMessage, String key)
-    {
-        try
-        {
-            setKey(key);
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedMessage)));
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-        return null;
+    public String decrypt(String encryptedMessage, String key) throws Exception {
+        setKey(key);
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedMessage)));
     }
 
-    public void setKey(String inputKey)
-    {
+    public void setKey(String inputKey) throws Exception {
         MessageDigest sha;
 
-        try
-        {
-            byte[] key = inputKey.getBytes(StandardCharsets.UTF_8);
-            sha = MessageDigest.getInstance("SHA-1");
-            key = sha.digest(key);
-            key = Arrays.copyOf(key, 16);
-            secretKey = new SecretKeySpec(key, "AES");
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
+        byte[] key = inputKey.getBytes(StandardCharsets.UTF_8);
+        sha = MessageDigest.getInstance("SHA-1");
+        key = sha.digest(key);
+        key = Arrays.copyOf(key, 16);
+        secretKey = new SecretKeySpec(key, "AES");
     }
 
-    public String encrypt(String plainMessage, String key)
-    {
-        try
-        {
-            setKey(key);
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(plainMessage.getBytes(StandardCharsets.UTF_8)));
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-        return null;
+    public String encrypt(String plainMessage, String key) throws Exception {
+        setKey(key);
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(plainMessage.getBytes(StandardCharsets.UTF_8)));
     }
 }
--- airport-security-control/src/main/java/Main/ext/builder/Application.java
+++ airport-security-control/src/main/java/Main/ext/builder/Application.java
@@ -1,6 +1,6 @@
 package Main.ext.builder;
 
-public class Application
+public class Application{
 
     public static void main(String... args)
     {
@@ -9,7 +9,5 @@ public class Application
                 .pepperoni(true)
                 .bacon(true)
                 .build();
-
-        System.out.println(pizza);
     }
 }
